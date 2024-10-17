5e4f216fc6ad94b6f00bfad04713ac271748a458
Tanish Baidya <tanish.baidya@gmail.com>
2024-10-17 22:19:03 +0100

    🚧 Add dev env containerisation and hot reloads

--- run.sh
+++ run.sh
@@ -8,80 +8,29 @@ shift 2
 
 case "$command" in
     
-    "docker-build:base")
-        docker build -f Dockerfile.base -t biaplanner/base:1.0.0 . --target base
+    "build:dev")
+        docker-compose -f docker-compose.dev.yaml build
     ;;
     
-    "docker-build:builder")
-        docker build -f Dockerfile.base -t biaplanner/builder:1.0.0 . --target builder
+    "build:prod")
+        docker-compose -f docker-compose.yaml build
     ;;
     
-    "docker-build:app")
-        docker build -f ./packages/app/Dockerfile -t biaplanner/app:1.0.0 . --target app
+    "start:dev")
+        docker-compose -f docker-compose.dev.yaml up
     ;;
-    # "install")
-    #     npm install
-    # ;;
     
-    # "install:rosid")
-    #     npm install "$@"
-    # ;;
+    "start:prod")
+        docker-compose -f docker-compose.yaml up
+    ;;
     
-    # "install:app")
-    #     npm install "$@" --workspace="app"
-    # ;;
+    "end:dev")
+        docker-compose -f docker-compose.dev.yaml down
+    ;;
     
-    # "install:server")
-    #     npm install "$@" --workspace="server"
-    # ;;
-    
-    # "install:shared")
-    #     npm install "$@" --workspace="shared"
-    # ;;
-    
-    # "start")
-    #     docker-compose up
-    # ;;
-    
-    # "lint-format")
-    #     ./run.sh format
-    #     ./run.sh lint
-    # ;;
-    
-    # "lint")
-    #     npm --prefix ./app run lint
-    #     npm --prefix ./server run lint
-    #     npm --prefix ./shared run lint
-    # ;;
-    
-    # "format")
-    #     npm --prefix ./app run format
-    #     npm --prefix ./server run format
-    #     npm --prefix ./shared run format
-    # ;;
-    
-    # "build:shared")
-    #     npm --prefix ./shared run build
-    #     docker build -t rosid/shared:1.0 ./shared
-    # ;;
-    
-    # "build:dependencies")
-    #     docker build -f ./Dockerfile.base -t rosid/dependencies:1.0 .
-    # ;;
-    
-    # "build:rosid")
-    #     docker-compose build
-    # ;;
-    
-    # "build:all")
-    #     # ./run.sh build:shared
-    #     ./run.sh build:dependencies
-    #     ./run.sh build:rosid
-    # ;;
-    
-    # "end")
-    #     docker-compose down
-    # ;;
+    "end:prod")
+        docker-compose -f docker-compose.yaml down
+    ;;
     
     *)
         echo "No valid option was chosen. Please select a valid choice from start, build, end"
