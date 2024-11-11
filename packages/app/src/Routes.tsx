b21a2af64920df828de9d1ac165e98a82d4ad8f3
Tanish Baidya <tbaidya01@qub.ac.uk>
2024-11-11 14:05:14 +0000

    🚚 Make sub feature folder name follow the underscore prefix pattern

--- packages/app/src/Routes.tsx
+++ packages/app/src/Routes.tsx
@@ -1,7 +1,7 @@
 import { Route, BrowserRouter as Router, Routes as RoutingTable } from "react-router-dom";
 
 import HomePage from "@/pages/HomePage";
-import InventoryPage from "./features/pantry-management/inventory/pages/InventoryPage";
+import InventoryPage from "./features/_pantry-management/_inventory/pages/InventoryPage";
 import LoginPage from "./features/authentication/pages/LoginPage";
 import Protected from "./features/authentication/components/Protected";
 import SignUpPage from "./features/authentication/pages/SignUpPage";
