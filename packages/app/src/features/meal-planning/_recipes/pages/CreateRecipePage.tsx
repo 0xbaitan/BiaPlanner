80f0bc93d347d152aa8f43f13784cfbcf7d8dbc4
Tanish Baidya <tbaidya01@qub.ac.uk>
2025-03-09 15:01:22 +0000

    ✨ Add status toast for the create recipe page

--- packages/app/src/features/meal-planning/_recipes/pages/CreateRecipePage.tsx
+++ packages/app/src/features/meal-planning/_recipes/pages/CreateRecipePage.tsx
@@ -1,21 +1,39 @@
-import { ICreateRecipeDto } from "@biaplanner/shared";
+import { ICreateRecipeDto, IRecipe } from "@biaplanner/shared";
+import useDefaultStatusToast, { Action } from "@/hooks/useDefaultStatusToast";
+
 import RecipeForm from "../components/RecipeForm";
+import { Status } from "@/hooks/useStatusToast";
 import { useCreateRecipeMutation } from "@/apis/RecipeApi";
 
 export default function CreateRecipePage() {
   const [createRecipeMutation, { isLoading, isError, isSuccess }] = useCreateRecipeMutation();
+
+  const { setItem } = useDefaultStatusToast<IRecipe>({
+    action: Action.CREATE,
+    entityIdentifier: (entity) => entity.title,
+    idPrefix: "recipes",
+    isError,
+    isLoading,
+    isSuccess,
+    idSelector: (entity) => entity?.id ?? "new",
+    toastProps: {
+      autoClose: 5000,
+    },
+    redirectContent: {
+      applicableStatuses: [Status.SUCCESS],
+      redirectUrl: "/meal-planning/recipes",
+      redirectButtonText: "Return to recipes",
+    },
+  });
+
   return (
     <div>
       <h1 className="bp-h1">Create Recipe</h1>
 
-      {isLoading && <div>Loading...</div>}
-
-      {isError && <div>Error</div>}
-
-      {isSuccess && <div>Recipe created successfully</div>}
       <RecipeForm
         type="create"
         onSubmit={async (values) => {
+          setItem(values as IRecipe);
           await createRecipeMutation(values as ICreateRecipeDto);
         }}
       />
