cd704d41c545a764b3add4d55650fd2591f29225
Tanish Baidya <tbaidya01@qub.ac.uk>
2025-03-17 10:08:09 +0000

    💄 Divide the meal plan form into two panels

--- packages/app/src/features/meal-planning/_meal-plans/components/MealPlanForm.tsx
+++ packages/app/src/features/meal-planning/_meal-plans/components/MealPlanForm.tsx
@@ -1,10 +1,15 @@
+import "../styles/MealPlanForm.scss";
+
 import { FormProvider, useForm } from "react-hook-form";
 import { IConcreteRecipe, ICreateConcreteRecipeDto, IUpdateConcreteRecipeDto } from "@biaplanner/shared";
 
 import Button from "react-bootstrap/esm/Button";
+import Col from "react-bootstrap/Col";
 import ConcreteIngredientListInput from "./ConcreteIngredientListInput";
+import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import MealTypeSelect from "./MealTypeSelect";
+import Row from "react-bootstrap/Row";
 
 export type ConcreteRecipeFormValues = ICreateConcreteRecipeDto | IUpdateConcreteRecipeDto;
 
@@ -28,19 +33,29 @@ export default function MealPlanForm(props: MealPlanFormValues) {
       <h2>Meal Plan Page Form</h2>
       <FormProvider {...methods}>
         <Form
+          className="bp-meal_plan_form"
           onSubmit={handleSubmit(() => {
             const values = getValues();
             reset();
             onSubmit(values);
           })}
         >
-          <MealTypeSelect
-            onChange={(mealType) => {
-              setValue("mealType", mealType);
-            }}
-          />
-          <ConcreteIngredientListInput />
-          <Button type="submit">Submit</Button>
+          <Container fluid>
+            <Row className="bp-meal_plan_form__dual_panel">
+              <Col md={4} className="bp-meal_plan_form__dual_panel__pane">
+                <MealTypeSelect
+                  onChange={(mealType) => {
+                    setValue("mealType", mealType);
+                  }}
+                />
+                <ConcreteIngredientListInput />
+              </Col>
+              <Col className="bp-meal_plan_form__dual_panel__pane">
+                {" "}
+                <Button type="submit">Submit</Button>
+              </Col>
+            </Row>
+          </Container>
         </Form>
       </FormProvider>
     </div>
